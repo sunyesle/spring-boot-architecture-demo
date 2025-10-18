bea800e38d592ed8116e98d244dca6ad54d877b6
sunyesle <sunyesle@gmail.com>
2025-10-18 14:33:25 +0900

    microservice: service-request-service 모듈 추가

---
+++ microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/ServiceRequestApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.microservices.service_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRequestApplication.class, args);
+	}
+
+}
---
+++ microservices/service-request-service/src/test/java/com/sunyesle/microservices/service_request/ServiceRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.microservices.service_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
