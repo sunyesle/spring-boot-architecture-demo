c52ab531b2b1a0fee3cf16c4a09ff7e3dc5d19b5
sunyesle <sunyesle@gmail.com>
2025-10-20 19:25:37 +0900

    microservice: 상담 이력 목록 조회 시 부서명 일괄 조회 및 매핑 로직 추가

--- microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/ServiceRequestBatchService.java
+++ microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/ServiceRequestBatchService.java
@@ -2,6 +2,7 @@ package com.sunyesle.microservices.service_request;
 
 import com.sunyesle.microservices.service_request.client.code.Code;
 import com.sunyesle.microservices.service_request.client.code.CodeClient;
+import com.sunyesle.microservices.service_request.client.department.DepartmentClient;
 import com.sunyesle.microservices.service_request.client.user.User;
 import com.sunyesle.microservices.service_request.client.user.UserClient;
 import lombok.RequiredArgsConstructor;
@@ -16,24 +17,29 @@ public class ServiceRequestBatchService {
     private final ServiceRequestMapper serviceRequestMapper;
     private final CodeClient codeClient;
     private final UserClient userClient;
+    private final DepartmentClient departmentClient;
 
     public List<ServiceRequest> getAll(int limit, int offset) {
         List<ServiceRequest> serviceRequests = serviceRequestMapper.selectAll(limit, offset);
 
         Set<String> userIds = new HashSet<>();
+        Set<String> departmentIds = new HashSet<>();
         for (ServiceRequest sr : serviceRequests) {
             userIds.add(sr.getCallAgentId());
             userIds.add(sr.getVocAssigneeId());
+            departmentIds.add(sr.getVocAssigneeDeptId());
         }
 
         Map<String, Map<String, String>> codeTypes = getCodeNamesByTypes("SR_TYPE", "SR_STATUS");
         Map<String, String> userNames = getUserNames(userIds);
+        Map<String, String> departmentNames = getDepartmentNames(departmentIds);
 
         for (ServiceRequest sr : serviceRequests) {
             sr.setTypeName(codeTypes.get("SR_TYPE").get(sr.getType()));
             sr.setStatusName(codeTypes.get("SR_STATUS").get(sr.getStatus()));
             sr.setCallAgentName(userNames.get(sr.getCallAgentId()));
             sr.setVocAssigneeName(userNames.get(sr.getVocAssigneeId()));
+            sr.setVocAssigneeDeptName(departmentNames.get(sr.getVocAssigneeDeptId()));
         }
         return serviceRequests;
     }
@@ -54,6 +60,14 @@ public class ServiceRequestBatchService {
 
     private Map<String, String> getUserNames(Set<String> userIds) {
         return userClient.getUsers(userIds).stream()
+                .collect(Collectors.toMap(
+                        User::getId,
+                        User::getName
+                ));
+    }
+
+    private Map<String, String> getDepartmentNames(Set<String> departmentIds) {
+        return departmentClient.getDepartments(departmentIds).stream()
                 .collect(Collectors.toMap(
                         User::getId,
                         User::getName
--- microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/client/department/DepartmentClient.java
+++ microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/client/department/DepartmentClient.java
@@ -1,9 +1,17 @@
 package com.sunyesle.microservices.service_request.client.department;
 
+import com.sunyesle.microservices.service_request.client.user.User;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
+import java.util.Set;
 
 public interface DepartmentClient {
     @GetExchange("/api/departments/{id}")
     Department getDepartment(@PathVariable String id);
+
+    @GetExchange("/api/departments")
+    List<User> getDepartments(@RequestParam Set<String> ids);
 }
