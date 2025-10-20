5bb679d4410be5592fc7501767cb96bc56466c69
sunyesle <sunyesle@gmail.com>
2025-10-20 19:06:38 +0900

    microservice: 상담 이력 목록 조회 시 사용자명 일괄 조회 및 매핑 로직 추가

--- microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/ServiceRequestBatchService.java
+++ microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/ServiceRequestBatchService.java
@@ -2,12 +2,12 @@ package com.sunyesle.microservices.service_request;
 
 import com.sunyesle.microservices.service_request.client.code.Code;
 import com.sunyesle.microservices.service_request.client.code.CodeClient;
+import com.sunyesle.microservices.service_request.client.user.User;
+import com.sunyesle.microservices.service_request.client.user.UserClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -15,15 +15,25 @@ import java.util.stream.Collectors;
 public class ServiceRequestBatchService {
     private final ServiceRequestMapper serviceRequestMapper;
     private final CodeClient codeClient;
+    private final UserClient userClient;
 
     public List<ServiceRequest> getAll(int limit, int offset) {
         List<ServiceRequest> serviceRequests = serviceRequestMapper.selectAll(limit, offset);
 
+        Set<String> userIds = new HashSet<>();
+        for (ServiceRequest sr : serviceRequests) {
+            userIds.add(sr.getCallAgentId());
+            userIds.add(sr.getVocAssigneeId());
+        }
+
         Map<String, Map<String, String>> codeTypes = getCodeNamesByTypes("SR_TYPE", "SR_STATUS");
+        Map<String, String> userNames = getUserNames(userIds);
 
         for (ServiceRequest sr : serviceRequests) {
             sr.setTypeName(codeTypes.get("SR_TYPE").get(sr.getType()));
             sr.setStatusName(codeTypes.get("SR_STATUS").get(sr.getStatus()));
+            sr.setCallAgentName(userNames.get(sr.getCallAgentId()));
+            sr.setVocAssigneeName(userNames.get(sr.getVocAssigneeId()));
         }
         return serviceRequests;
     }
@@ -39,6 +49,14 @@ public class ServiceRequestBatchService {
                                         Code.CodeItem::getCode,
                                         Code.CodeItem::getValue
                                 ))
+                ));
+    }
+
+    private Map<String, String> getUserNames(Set<String> userIds) {
+        return userClient.getUsers(userIds).stream()
+                .collect(Collectors.toMap(
+                        User::getId,
+                        User::getName
                 ));
     }
 }
--- microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/client/user/UserClient.java
+++ microservices/service-request-service/src/main/java/com/sunyesle/microservices/service_request/client/user/UserClient.java
@@ -1,9 +1,16 @@
 package com.sunyesle.microservices.service_request.client.user;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
+import java.util.Set;
 
 public interface UserClient {
     @GetExchange("/api/users/{id}")
     User getUser(@PathVariable String id);
+
+    @GetExchange("/api/users")
+    List<User> getUsers(@RequestParam Set<String> ids);
 }
